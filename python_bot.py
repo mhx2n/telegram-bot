ce75426046733a9a6a3c4000eacd95ec06f042e9
mhx2n <mhdiv2331@gmail.com>
2025-12-18 22:36:21 +0600

    Update python_bot.py

--- python_bot.py
+++ python_bot.py
@@ -1,16 +1,52 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
-import os
+from aiogram.filters import Command
 
-BOT_TOKEN = os.getenv("8318888870:AAG_HjP0ucgmq4zDUKsXgEFjj5371LffnZI")
+BOT_TOKEN = "8318888870:AAG_HjP0ucgmq4zDUKsXgEFjj5371LffnZI"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-@dp.message()
-async def echo(msg: Message):
-    await msg.answer("Bot is running on Render ✅")
+# start command
+@dp.message(Command("start"))
+async def start_cmd(message: Message):
+    await message.reply(
+        "📁 File Rename Bot\n\n"
+        "👉 যেকোনো ফাইল পাঠাও\n"
+        "👉 তারপর নতুন নাম পাঠাও (extension সহ)\n\n"
+        "Example:\nmy_file.pdf"
+    )
+
+# store last file
+user_files = {}
+
+@dp.message(F.document | F.video | F.audio)
+async def get_file(message: Message):
+    file = message.document or message.video or message.audio
+    user_files[message.from_user.id] = file
+    await message.reply("✏️ নতুন ফাইল নাম পাঠাও (extension সহ)")
+
+@dp.message(F.text)
+async def rename_file(message: Message):
+    user_id = message.from_user.id
+
+    if user_id not in user_files:
+        return
+
+    new_name = message.text.strip()
+    file = user_files[user_id]
+
+    await bot.send_chat_action(message.chat.id, "upload_document")
+
+    await bot.send_document(
+        chat_id=message.chat.id,
+        document=file.file_id,
+        caption="✅ Renamed Successfully",
+        filename=new_name
+    )
+
+    del user_files[user_id]
 
 async def main():
     await dp.start_polling(bot)
